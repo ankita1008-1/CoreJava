324313ff91be40eaf1e02eb315ae6d02980f97c5
ankita1008-1 <chougaleankita0@gmail.com>
2025-11-20 19:18:00 +0530

    Add files via upload

---
+++ EWIT_Project/src/com/tnsif/basic/SLInheritance.java
@@ -0,0 +1,12 @@
+package com.tnsif.SLInheritance;
+
+public class SLInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student obj = new Student("Ankita","0123","Bengaluru",1234L,123,"EWIT");
+		System.out.println(obj);
+
+	}
+
+}
