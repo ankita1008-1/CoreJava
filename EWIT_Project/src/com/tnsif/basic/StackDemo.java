bc2e51fa01f2c3286a44b4b21ee0b35e5bfa975b
ankita1008-1 <chougaleankita0@gmail.com>
2025-11-20 19:21:04 +0530

    Add files via upload

---
+++ EWIT_Project/src/com/tnsif/basic/StackDemo.java
@@ -0,0 +1,20 @@
+package com.tns.collections.List;
+import java.util.Deque;
+import java.util.Stack;
+import java.util.Vector;
+public class StackDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack s=new Stack();
+		s.push(10);
+		s.push(20);
+		System.out.println(s);
+		s.pop();
+		System.out.println(s);
+	}
+
+
+	}
+
+
---
+++ EWIT_Project/src/com/tnsif/basic/VectorDemo.java
@@ -0,0 +1,12 @@
+package com.tns.collections.List;
+import java.util.Vector;
+
+public class VectorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vector v=new Vector();
+
+	}
+
+}
