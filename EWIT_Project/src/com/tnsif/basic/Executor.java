aca0ec49a87346de140616f2aaa5a0454eadd180
ankita1008-1 <chougaleankita0@gmail.com>
2025-11-20 19:16:21 +0530

    Add files via upload

---
+++ EWIT_Project/src/com/tnsif/basic/Executor.java
@@ -0,0 +1,24 @@
+package com.tnsif.firstpackage1;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p1=new Person();
+		p1.personAge = 22;
+		p1.personCity = "Bengaluru";
+		p1.setPersonName("Ankita");
+		
+		p1.display();
+		
+		Person p2=new Person();
+		p2.display();
+
+		Person p3=new Person();
+		p3.display();
+	}
+
+
+	}
+
+
---
+++ EWIT_Project/src/com/tnsif/basic/Person.java
@@ -0,0 +1,33 @@
+package com.tnsif.firstpackage1;
+
+public class Person {
+	private String personName;
+	protected int personAge;
+	String personCity;
+	
+	//Go to Source Generate Getters and Setters Select all
+	public String getPersonName() {
+		return personName;
+	}
+	public void setPersonName(String personName) {
+		this.personName=personName;
+	}
+	public int getPersonAge() {
+		return personAge;
+	}
+	public void setPersonAge(int personAge) {
+		this.personAge=personAge;
+	}
+	public String getPersonCity() {
+		return personCity;
+	}
+	public void setPersonCity(String personCity) {
+		this.personCity=personCity;
+	}
+	
+	void display() {
+		System.out.println(personName+" "+personAge+" "+personCity);
+	}	
+}
+
+
